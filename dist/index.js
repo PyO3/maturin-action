2451c82191a4ee51074c06456ab137a0e17d03a7
messense <messense@icloud.com>
2022-08-13 09:10:31 +0800

    Defaults to manylinux2014 when using `auto` with Rust beta/nightly

--- dist/index.js
+++ dist/index.js
@@ -11580,9 +11580,21 @@ async function installMaturin(tag) {
         return exe;
     }
 }
+function autoManylinuxVersion(manylinux, rustToolchain, target) {
+    if (manylinux === 'auto' &&
+        !target.includes('musl') &&
+        (rustToolchain.startsWith('beta') || rustToolchain.startsWith('nightly'))) {
+        return '2014';
+    }
+    else {
+        return manylinux;
+    }
+}
 async function dockerBuild(tag, manylinux, args) {
     var _a;
     const target = getRustTarget(args);
+    const rustToolchain = (await getRustToolchain(args)) || 'stable';
+    manylinux = autoManylinuxVersion(manylinux, rustToolchain, target);
     let container = core.getInput('container');
     if (container.length === 0) {
         container =
@@ -11623,7 +11635,6 @@ async function dockerBuild(tag, manylinux, args) {
     const url = tag === 'latest'
         ? `https://github.com/PyO3/maturin/releases/latest/download/maturin-${arch}-unknown-linux-musl.tar.gz`
         : `https://github.com/PyO3/maturin/releases/download/${tag}/maturin-${arch}-unknown-linux-musl.tar.gz`;
-    const rustToolchain = (await getRustToolchain(args)) || 'stable';
     const rustupComponents = core.getInput('rustup-components');
     const commands = [
         '#!/bin/bash',
