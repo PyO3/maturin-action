7c069cda92449cfe3782abf44db7404a7bb7e6b2
messense <messense@icloud.com>
2022-12-08 15:52:13 +0800

    Remove hardcoded `MACOSX_DEPLOYMENT_TARGET`

--- src/index.ts
+++ src/index.ts
@@ -702,10 +702,13 @@ async function innerMain(): Promise<void> {
         await installRustTarget('x86_64-apple-darwin', rustToolchain)
       }
       await installRustTarget('aarch64-apple-darwin', rustToolchain)
-      env.DEVELOPER_DIR = '/Applications/Xcode.app/Contents/Developer'
-      env.SDKROOT =
-        '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk'
-      env.MACOSX_DEPLOYMENT_TARGET = '10.9'
+      if (!env.DEVELOPER_DIR) {
+        env.DEVELOPER_DIR = '/Applications/Xcode.app/Contents/Developer'
+      }
+      if (!env.SDKROOT) {
+        env.SDKROOT =
+          '/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk'
+      }
       core.endGroup()
     }
 
